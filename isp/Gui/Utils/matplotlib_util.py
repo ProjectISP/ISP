59b7f9e28671a57252b6acea1011782198a99ec0
junqfisica <correrjunqueira@uni-potsdam.de>
2019-12-11 16:22:06 +0100

    Thiago - Added few more key names to keys.

--- isp/Gui/Utils/matplotlib_util.py
+++ isp/Gui/Utils/matplotlib_util.py
@@ -1,19 +1,23 @@
 
 
-class Key:
+class Keys:
+    NoKey = "None"
     Ctr = "control"
     Shift = "shift"
+    Alt = "alt"
     Plus = "+"
     Minus = "-"
     Esc = "escape"
+    Up = "up"
+    Down = "down"
     # TODO include more keys as necessary
 
 
 # include more key names here if necessary.
-POSITIVE_POLARITY_KEYS = [Key.Plus, Key.Shift]
+POSITIVE_POLARITY_KEYS = [Keys.Plus, Keys.Shift]
 
 # include more key names here if necessary.
-NEGATIVE_POLARITY_KEYS = [Key.Minus, Key.Ctr]
+NEGATIVE_POLARITY_KEYS = [Keys.Minus, Keys.Ctr]
 
 
 def map_polarity_from_pressed_key(key_name: str):
