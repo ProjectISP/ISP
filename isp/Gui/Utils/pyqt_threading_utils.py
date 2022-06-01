a2ce3e2d24c85d04fcf5339aae3d0ca8e207d1b4
Roberto Cabieces Diaz <rcabdia@gmail.com>
2022-06-01 11:21:07 +0200

    Included utility for parallel workers and added example

--- isp/Gui/Utils/pyqt_threading_utils.py
+++ isp/Gui/Utils/pyqt_threading_utils.py
@@ -1,12 +1,14 @@
+import functools
 from multiprocessing.dummy import Pool as ThreadPool
 
-from PyQt5.QtCore import QObject, pyqtSignal, pyqtSlot, QThread
+from PyQt5.QtCore import QObject, pyqtSignal, pyqtSlot, QThread, pyqtBoundSignal
 
 
 class Worker(QObject):
-    finished = pyqtSignal(object)
-    progress = pyqtSignal(int)
-    exception = pyqtSignal(object)
+    finished: pyqtBoundSignal = pyqtSignal(object)
+    progress: pyqtBoundSignal = pyqtSignal(int)
+    exception: pyqtBoundSignal = pyqtSignal(object)
+
 
     def __init__(self):
         super().__init__()
@@ -56,9 +58,9 @@ class Worker(QObject):
 
 
 class ParallelWorkers(QObject):
-    finished = pyqtSignal()
-    progress = pyqtSignal(int, object)
-    exception = pyqtSignal(object)
+    finished: pyqtBoundSignal = pyqtSignal()
+    progress: pyqtBoundSignal = pyqtSignal(int, object)
+    exception: pyqtBoundSignal = pyqtSignal(object)
 
     def __init__(self, threads: int):
         super().__init__()
@@ -113,3 +115,20 @@ class ParallelWorkers(QObject):
         finally:
             self.finished.emit()
             self.thread.exit()
+
+def thread(on_finished: str = None):
+    def decorator_thread(func):
+        @functools.wraps(func)
+        def wrapper_thread(*args, **kwargs):
+            self = args[0]  # expected to be an object
+            on_finished_func = self.__getattribute__(on_finished)
+            worker_name = f"worker_{func.__name__}"
+            self.__setattr__(worker_name, Worker())  # sets a new worker for this method
+            worker = self.__getattribute__(worker_name)
+
+            worker.job(lambda: func(*args, **kwargs))
+            worker.job_finished(on_finished_func)
+            worker.start()
+
+        return wrapper_thread
+    return decorator_thread
