21c77636f911f8b8d171f6782a20903092a9e58e
Roberto Cabieces <rcabdia@gmail.com>
2024-10-26 14:52:25 +0200

    Adding more flexibility to running scripts and debugged comand + D to run it

---
+++ isp/scripts/__init__.py
@@ -0,0 +1,9 @@
+"""
+
+Here you can set the script you want to run in this case is script file.
+Mandatory is import run_process
+
+"""
+
+
+from .script import run_process
--- isp/scripts/script.py
+++ isp/scripts/script.py
@@ -1,23 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 """
 script.py
-
-
-
-:param : 
-:type : 
-:return: 
-:rtype: 
 """
 
-from obspy import Stream
+from obspy import Stream, UTCDateTime
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Qt5Agg')
+def run_process(st: Stream, starttime: UTCDateTime, endtime: UTCDateTime, hypo_lat: float, hypo_lon: float,
+                hypo_depth_km: float, hypo_origin_time: UTCDateTime):
 
-def run_process(st: Stream):
-    data = None
-    # running your script
-    #data = st[0].data+ st[1].data
-    #print(data)
-    return data
+    # example of hw to design your running script
+
+    try:
+
+        data = st[0].data + st[1].data
+
+        fig, ax1 = plt.subplots(1, 1, layout='constrained')
+        ax1.plot(st[0].times(), data)
+        ax1.set_xlabel('Time (s)')
+        ax1.set_ylabel('Trace1 + Trace')
+        ax1.grid(True)
+        plt.show()
+
+    except Exception as e:
+        # Handle any exception and print the error message
+        print("An error occurred:", str(e))
+
+
 
 
