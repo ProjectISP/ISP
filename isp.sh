960adfc9338f2b05fa4e500bfc82a12c89f4f933
rcabdia <rcabdia@gmail.com>
2025-02-25 14:22:06 +0100

    modified isp.sh to detect wether your system has wayland instead of X11 windows server. Designed Ubuntu 24.04

--- isp.sh
+++ isp.sh
@@ -6,11 +6,16 @@ ISP_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 source $(conda info --base)/etc/profile.d/conda.sh
 
 if [[ `uname -s` == "Darwin" ]]; then
-	export OS="MacOSX"
-	source activate isp
+    export OS="MacOSX"
+    source activate isp
 else
-	export OS="Linux"
-	conda activate isp
+    export OS="Linux"
+    conda activate isp
+fi
+
+# Detectar si estamos en una sesión Wayland
+if [[ "$XDG_SESSION_TYPE" == "wayland" ]]; then
+    export QT_QPA_PLATFORM=wayland
 fi
 
 pushd ${ISP_DIR} > /dev/null
