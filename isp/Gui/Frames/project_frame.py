8d05623c9d8548ce1766d3fc68321798e1a1dea4
rcabdia <rcabdia@gmail.com>
2025-09-25 12:11:14 +0200

    Problems related to project creation solved

--- isp/Gui/Frames/project_frame.py
+++ isp/Gui/Frames/project_frame.py
@@ -1,9 +1,6 @@
 import os
 import pickle
-from concurrent.futures.thread import ThreadPoolExecutor
-
 from surfquakecore.project.surf_project import SurfProject
-
 from isp import ROOT_DIR
 from isp.Gui import pw, pqg, pyc
 from isp.Gui.Frames import MessageDialog
@@ -12,6 +9,29 @@ from isp.Gui.Utils.pyqt_utils import BindPyqtObject
 from sys import platform
 from isp.Utils import MseedUtil
 
+
+class WorkerSignals(pyc.QObject):
+    finished = pyc.pyqtSignal()
+    error = pyc.pyqtSignal(str)
+    result = pyc.pyqtSignal(object)
+
+class WorkerRunnable(pyc.QRunnable):
+    def __init__(self, fn, *args, **kwargs):
+        super().__init__()
+        self.fn = fn
+        self.args = args
+        self.kwargs = kwargs
+        self.signals = WorkerSignals()
+
+    def run(self):
+        try:
+            result = self.fn(*self.args, **self.kwargs)
+        except Exception as e:
+            self.signals.error.emit(str(e))
+        else:
+            self.signals.result.emit(result)
+        finally:
+            self.signals.finished.emit()
 
 class Project(pw.QDialog, UiProject):
 
@@ -40,50 +60,32 @@ class Project(pw.QDialog, UiProject):
         self.progressbar.setValue(self.progressbar.value() + 1)
 
     def onChange_root_path(self, value):
-        """
-        Fired every time the root_path is changed
-
-        :param value: The path of the new directory.
-
-        :return:
-        """
         pass
 
     def load_files_done(self):
-
-        info = {}
         if self.rootPathForm_inv.text() == "":
             filter = "All files (*.*)"
         else:
-
             filter = self.rootPathForm_inv.text()
-        print(filter)
 
         selected_files, _ = pw.QFileDialog.getOpenFileNames(self, "Select Project", ROOT_DIR, filter=filter)
 
         md = MessageDialog(self)
-        md.hide()
 
-        try:
-            ms = MseedUtil()
-            self.progressbar.reset()
-            self.progressbar.setLabelText("Bulding Project")
-            self.progressbar.setRange(0, 0)
+        ms = MseedUtil()
+        self.progressbar.reset()
+        self.progressbar.setLabelText("Creating project")
+        self.progressbar.setRange(0, 0)
+        self.progressbar.show()
 
-            def callback():
-                r = ms.search_indiv_files(selected_files)
-                pyc.QMetaObject.invokeMethod(self.progressbar, "accept")
-                return r
+        def background_task():
+            return ms.search_indiv_files(selected_files)
 
-            with ThreadPoolExecutor(1) as executor:
-                f = executor.submit(callback)
-                self.progressbar.exec()
-                self.project = f.result()
-                f.cancel()
-
+        def on_result(result):
+            self.project = result
             info = ms.get_project_basic_info(self.project)
 
-            if len(info)>0:
+            if len(info) > 0:
                 md.set_info_message("Loaded Project ",
                                     "Networks: " + ','.join(info["Networks"][0]) + "\n" +
                                     "Stations: " + ','.join(info["Stations"][0]) + "\n" +
@@ -93,21 +95,27 @@ class Project(pw.QDialog, UiProject):
                                     "Stations Number: " + str(info["Stations"][1]) + "\n" +
                                     "Channels Number: " + str(info["Channels"][1]) + "\n" +
                                     "Num Files: " + str(info["num_files"]) + "\n")
-
             else:
-                md.set_warning_message("Empty Project ", "Please provide a root path "
-                                                         "with mseed files inside and check the wuery filters applied")
+                md.set_warning_message("Empty Project ",
+                                       "Please provide a root path with mseed files inside and check the query filters applied")
 
-        except:
+            md.show()
+            self.progressbar.hide()
 
-            md.set_error_message("Something went wrong. Please check that your data files are correct mseed files")
+        def on_error(error_msg):
+            md.set_error_message("Something went wrong. Please check your data files are correct mseed files.\n" + error_msg)
+            md.show()
+            self.progressbar.hide()
 
-        md.show()
+        runnable = WorkerRunnable(background_task)
+        runnable.signals.result.connect(on_result)
+        runnable.signals.error.connect(on_error)
+        runnable.signals.finished.connect(self.progressbar.hide)
 
+        pyc.QThreadPool.globalInstance().start(runnable)
 
 
     def on_click_select_directory(self, bind: BindPyqtObject):
-
         if "darwin" == platform:
             dir_path = pw.QFileDialog.getExistingDirectory(self, 'Select Directory', bind.value)
         else:
@@ -122,26 +130,21 @@ class Project(pw.QDialog, UiProject):
         info = {}
         md = MessageDialog(self)
         md.hide()
-        try:
-            ms = MseedUtil()
-            self.progressbar.reset()
-            self.progressbar.setLabelText("Bulding Project")
-            self.progressbar.setRange(0, 0)
-            def callback():
-                r = ms.search_files(self.root_path_bind.value)
-                pyc.QMetaObject.invokeMethod(self.progressbar, "accept")
-                return r
-            with ThreadPoolExecutor(1) as executor:
-                f = executor.submit(callback)
-                self.progressbar.exec()
-                self.project = f.result()
-                f.cancel()
+        ms = MseedUtil()
 
+        self.progressbar.reset()
+        self.progressbar.setLabelText("Building Project")
+        self.progressbar.setRange(0, 0)
+        self.progressbar.show()
 
+        def background_task():
+            return ms.search_files(self.root_path_bind.value)
+
+        def on_result(result):
+            self.project = result
             info = ms.get_project_basic_info(self.project)
 
             if len(info) > 0:
-
                 md.set_info_message("Opened Project ",
                                     "Networks: " + ','.join(info["Networks"][0]) + "\n" +
                                     "Stations: " + ','.join(info["Stations"][0]) + "\n" +
@@ -151,22 +154,29 @@ class Project(pw.QDialog, UiProject):
                                     "Stations Number: " + str(info["Stations"][1]) + "\n" +
                                     "Channels Number: " + str(info["Channels"][1]) + "\n" +
                                     "Num Files: " + str(info["num_files"]) + "\n")
-
             else:
-                md.set_warning_message("Empty Project ", "Please provide a root path "
-                                                         "with mseed files inside and check the wuery filters applied")
+                md.set_warning_message("Empty Project ",
+                                       "Please provide a root path with mseed files inside and check the query filters applied")
 
-        except:
+            md.show()
+            self.progressbar.hide()
 
-            md.set_error_message("Something went wrong. Please check that your data files are correct mseed files")
+        def on_error(error_msg):
+            md.set_error_message("Something went wrong. Please check that your data files are correct mseed files\n" + error_msg)
+            md.show()
+            self.progressbar.hide()
 
-        md.show()
+        runnable = WorkerRunnable(background_task)
+        runnable.signals.result.connect(on_result)
+        runnable.signals.error.connect(on_error)
+        runnable.signals.finished.connect(self.progressbar.hide)
 
+        pyc.QThreadPool.globalInstance().start(runnable)
 
 
     def saveProject(self):
 
-        if len(self.project) >0:
+        if hasattr(self, 'project') and len(self.project) > 0:
             try:
                 if "darwin" == platform:
                     path = pw.QFileDialog.getExistingDirectory(self, 'Select Directory', self.root_path_bind.value)
@@ -176,21 +186,23 @@ class Project(pw.QDialog, UiProject):
                 if not path:
                     return
 
-                file_to_store = open(os.path.join(path,self.nameForm.text()), "wb")
+                file_to_store = open(os.path.join(path, self.nameForm.text()), "wb")
 
                 # Now writes surfQuake Project
                 sp = SurfProject("")
                 sp.project = self.project
                 sp.data_files = MseedUtil.generate_data_files(self.project)
                 pickle.dump(sp, file_to_store)
-    
+
                 md = MessageDialog(self)
                 md.set_info_message("Project saved successfully")
+                md.show()
 
-            except:
-
+            except Exception as e:
                 md = MessageDialog(self)
-                md.set_info_message("No data to save in Project")
+                md.set_error_message("Failed to save project: " + str(e))
+                md.show()
         else:
             md = MessageDialog(self)
-            md.set_info_message("No data to save in Project")
+            md.set_warning_message("No data to save in Project")
+            md.show()
